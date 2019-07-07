80f8751918a4251d58767eaace5688c456fc3158
Abhishek Tiwari <dev.abhishekt@gmail.com>
2019-07-07 23:36:09 +0530

    first commit

---
+++ amazonPrice.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+def checkPrice() :
+   URL = 'https://www.amazon.in/Tissot-T0674171105100-Wrist-Watch-Men/dp/B0051DA94I/ref=sr_1_4_sspa?keywords=wrist+watch&qid=1562519463&rnid=3576079031&s=watch&sr=1-4-spons&psc=1'
+   headers = {"User-agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+
+   page = requests.get(URL,headers=headers)
+
+   soup = BeautifulSoup(page.content,'html.parser')
+
+   title = soup.find(id="productTitle").get_text()
+   price = soup.find(id="priceblock_ourprice").get_text()
+
+   converted_price = price[2:4]
+
+   print(converted_price)
+
+checkPrice()
+
+
+
+
